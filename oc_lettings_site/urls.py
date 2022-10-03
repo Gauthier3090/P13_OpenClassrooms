5ac836672caf191d105c25ac60b05da2eecaca9d
Gauthier <gauthier.pladet@gmail.com>
2022-10-03 20:41:25 +0200

    Add css home index

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from . import views
@@ -19,3 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('sentry-debug/', trigger_error),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
