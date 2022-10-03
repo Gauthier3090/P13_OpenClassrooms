e9930be5f28341dc7bddd3adabf4e3c4986560c4
Gauthier <gauthier.pladet@gmail.com>
2022-10-03 15:57:38 +0200

    Change settings.py

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,6 +1,9 @@
 import os
 import sentry_sdk
 import environ
+from django.core.management.utils import get_random_secret_key
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from sentry_sdk.integrations.django import DjangoIntegration
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -8,8 +11,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
+SECRET_KEY = env('DJANGO_SECRET_KEY', default=get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(env('DEBUG', default=0))
