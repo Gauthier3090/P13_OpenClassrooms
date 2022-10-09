040ea0e446b162f0a36084e6b6f965624b497418
Gauthier <gauthier.pladet@gmail.com>
2022-10-09 18:07:32 +0200

    add css

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,7 +116,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+else:
+    STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_ROOT = [os.path.join(BASE_DIR, 'media')]
 
 mimetypes.add_type("text/css", ".css", True)
 
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,11 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.urls import re_path
 from django.urls import path, include
+from django.views.static import serve
+
 from . import views
 
 
-def trigger_error(request):
+def trigger_error():
     division_by_zero = 1 / 0
     return division_by_zero
 
@@ -20,6 +23,8 @@ urlpatterns = [
     path('profiles/', include('profiles.urls')),
     path('admin/', admin.site.urls),
     path('sentry/', trigger_error),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
